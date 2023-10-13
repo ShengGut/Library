3f213cf2fea8194892ae8256bba52151498e8c2b
ShengGut <kazuna@Kazunas-MacBook-Pro.local>
2023-10-12 19:17:01 -0700

    Added html for rudimentary library

--- script.js
+++ script.js
@@ -21,7 +21,32 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function displayBook(myLibrary) {
+    const display = document.getElementById("displayLibrary");
+    display.textContent = "Library:";
     for(let i = 0; i < myLibrary.length;i++) {
         console.log(myLibrary[i].info());
+        const bookInfo = myLibrary[i].info();
+        const bookElement = document.createElement("p");
+        bookElement.textContent = bookInfo;
+        display.appendChild(bookElement);
     }
 }
+displayBook(myLibrary);
+
+const form = document.getElementById("addBook");
+const submitButton = document.getElementById("submitButton");
+const output = document.getElementById("output");
+
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").value;
+
+    addBookToLibrary(title, author, pages, read);
+    displayBook(myLibrary);
+
+    alert(`Successfully added new book: ${title} by ${author}, ${pages}, ${read}`);
+})
