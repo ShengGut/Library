6028241872249b08ecc669d2d725abbcef7bf18d
ShengGut <kazuna@Kazunas-MacBook-Pro.local>
2023-10-19 17:07:10 -0700

    Added Javascript functions to delete book from library and toggle read status

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        return `${title} by ${author}, ${pages}, ${read}`;
+        return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
     }
 }
 
@@ -20,22 +20,64 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
 }
 
+function toggleReadStatus (index) {
+    const book = myLibrary[index];
+    const currentBookStatus = book.read.toLowerCase();
+    const newStatus = currentBookStatus === "read" ? "not read yet" : "read";
+    console.log(newStatus);
+    book.read = newStatus;
+    displayBook(myLibrary);
+}
+
 function displayBook(myLibrary) {
-    const display = document.getElementById("displayLibrary");
-    display.textContent = "Library:";
+    const bookList = document.getElementById("bookList");
+    bookList.innerHTML = "";
     for(let i = 0; i < myLibrary.length;i++) {
         console.log(myLibrary[i].info());
         const bookInfo = myLibrary[i].info();
-        const bookElement = document.createElement("p");
-        bookElement.textContent = bookInfo;
-        display.appendChild(bookElement);
+
+        const listItem = document.createElement("li");
+        
+        // create delete button and add to the list
+        const deleteButton = document.createElement("button")
+        deleteButton.textContent = "x";
+        deleteButton.setAttribute("data-index",i);
+        deleteButton.addEventListener("click", (event) => {
+            const index = parseInt(event.target.getAttribute("data-index"));
+            myLibrary.splice(index, 1);
+            displayBook(myLibrary);
+        });
+
+        // create read toggle button and add to the list
+        const toggleButton = document.createElement("button");
+        toggleButton.textContent = "read";
+        toggleButton.setAttribute("data-index", i);
+        toggleButton.addEventListener("click", (event) => {
+            const index = parseInt(event.target.getAttribute("data-index"));
+            toggleReadStatus(index);
+        });
+
+        listItem.appendChild(deleteButton);
+        const bookTextNode = document.createTextNode(bookInfo);
+        listItem.appendChild(bookTextNode);
+        listItem.appendChild(toggleButton);
+        bookList.appendChild(listItem);
+
     }
 }
 displayBook(myLibrary);
 
+const showFormButton = document.getElementById("showFormButton");
+const inputForm = document.getElementById("inputForm");
 const form = document.getElementById("addBook");
 const submitButton = document.getElementById("submitButton");
-const output = document.getElementById("output");
+
+showFormButton.addEventListener("click", () =>{
+    if(inputForm.style.display === "none")
+        inputForm.style.display = "block";
+    else
+        inputForm.style.display = "none";
+});
 
 submitButton.addEventListener("click", function(event) {
     event.preventDefault();
@@ -49,4 +91,5 @@ submitButton.addEventListener("click", function(event) {
     displayBook(myLibrary);
 
     alert(`Successfully added new book: ${title} by ${author}, ${pages}, ${read}`);
+    inputForm.style.display = "none";
 })
