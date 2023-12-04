79db187bdeceb6accec258ca9d5ba5d705f6f9fc
ShengGut <kazuna@Kazunas-MacBook-Pro.local>
2023-12-03 20:25:36 -0800

    Refactored to ES6 classes

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    info() {
+        return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
+    }
+}
+
 const myLibrary = [
     new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet"),
     new Book("Harry Potter and the Sorceror's Stone", "J.K. Rowling", "309 pages", "read"),
@@ -5,16 +18,6 @@ const myLibrary = [
     new Book("Assassin's Apprentice", "Robin Hobb", "448 pages", "read"),
     new Book("Lonesome Dove", "Larry McMurtry", "843 pages", "not read yet")
 ];
-
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
-    }
-}
 
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
